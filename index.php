ea1c19dbe80a89297d2d8b4a6ebf33138b903e7b
BitronicStudio <exemoya2016@gmail.com>
2025-10-04 23:19:45 -0300

    Agregado de style.css y actualizacion de index.php, diseño de formulario de inicio de sesion v1

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Underfall</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <div class="container-all">
+        <div class="ctn-form">
+            <img src="" alt="" class="logo">
+            <h1 class="title">INICIAR SESION</h1>
+            
+            <form action="">
+                <label for="">EMAIL</label>
+                <input type="text">
+                <label for="">CONTRASEÑA</label>
+                <input type="password">
+
+                <input type="submit" value="INICIAR">
+            </form>
+
+            <span class="text-footer">¿AUN NO TE HAS REGISTRADO? <a href="#">REGISTRATE</a></span>
+        </div>
+       
+        <div class="ctn-text">
+            <div class="capa">
+                <img class="logo-form" src="img/Logo-white.png" alt="">
+                <p class="text-description">REGISTRATE PARA RECIBIR MAS NOTIFICACIONES SOBRE EL DESARROLLO DE UNDERFALL</p>
+            </div>
+       </div>
+
+
+
+    </div>
+    
+</body>
+</html>
